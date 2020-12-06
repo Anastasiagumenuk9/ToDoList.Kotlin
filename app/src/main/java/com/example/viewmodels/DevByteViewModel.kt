7a39207e723a7211edb6cdb9dabfbf1355cfa672
Anastasiagumenuk9 <44962434+Anastasiagumenuk9@users.noreply.github.com>
2020-12-06 23:43:43 +0200

    Implemented a Repository to load data without specifying the data source

---
+++ app/src/main/java/com/example/viewmodels/DevByteViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.database.ToDoListDatabase.Companion.getInstance
+import com.example.repository.VideosRepository
+import kotlinx.coroutines.launch
+
+class DevByteViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val database = getInstance(application)
+    private val videosRepository = VideosRepository(database)
+
+    /**
+     * init{} is called immediately when this ViewModel is created.
+     */
+    init {
+        viewModelScope.launch {
+            videosRepository.refreshVideos()
+        }
+    }
+
+    val playlist = videosRepository.videos
+
+
+    class Factory(val app: Application) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(DevByteViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return DevByteViewModel(app) as T
+            }
+            throw IllegalArgumentException("Unable to construct viewmodel")
+        }
+    }
+}
